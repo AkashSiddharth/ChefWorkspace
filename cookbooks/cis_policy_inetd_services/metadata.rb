ec47d9a0a578acd05c64ae07325a65b4173b23c8
Akash Siddharth <14816427+AkashSiddharth@users.noreply.github.com>
2020-04-17 15:34:52 -0400

    Updating metadata and adding README

--- cookbooks/cis_policy_inetd_services/metadata.rb
+++ cookbooks/cis_policy_inetd_services/metadata.rb
@@ -6,6 +6,6 @@ description         'Enforces the CIS inetd service benchmarks'
 version             '0.1.0'
 chef_version        '>= 14.0'
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url          'https://github.com/akashsiddharth/' if respond_to?(:source_url)
-issues_url          'https://github.com/akashsiddharth/cassandra-chef-cookbook/issues' if respond_to?(:issues_url)
+source_url          'https://github.com/AkashSiddharth/ChefWorkspace/tree/Dev/cookbooks/cis_policy_inetd_services' if respond_to?(:source_url)
+issues_url          'https://github.com/AkashSiddharth/ChefWorkspace/tree/Dev/cookbooks/cis_policy_inetd_services/issues' if respond_to?(:issues_url)
 supports            ubuntu
